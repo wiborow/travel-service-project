8431600ea3fd273081ccdf6266decad71c0301f8
Wieslaw Borowski <wiborow@gmail.com>
2019-10-26 19:25:57 +0100

    Initial project

---
+++ src/main/java/com/sda/javadublin1/travelserviceproject/TravelServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sda.javadublin1.travelserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelServiceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelServiceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sda/javadublin1/travelserviceproject/TravelServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.javadublin1.travelserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
